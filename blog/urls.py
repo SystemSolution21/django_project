ca9b695fecc275e99783f3677757516f83b029b6
SystemSolution21 <systemsolution21@gmail.com>
2026-01-09 14:05:35 +0900

    new feature post_detail is added

--- blog/urls.py
+++ blog/urls.py
@@ -6,9 +6,16 @@ from django.urls.resolvers import URLPattern
 
 # Import local modules
 from . import views
+from .views import PostDetailView, PostListView
 
 # Define urlpatterns
 urlpatterns: list[URLPattern] = [
-    path(route="", view=views.home, name="blog-home"),
+    # path(route="", view=views.home, name="blog-home"), # function base view
+    path(
+        route="", view=PostListView.as_view(), name="blog-home"
+    ),  # class base Post list view
+    path(
+        route="post/<int:pk>/", view=PostDetailView.as_view(), name="post-detail"
+    ),  # Post detail view
     path(route="about/", view=views.about, name="blog-about"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -4,11 +4,13 @@
 from django.db.models.manager import BaseManager
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.views.generic import DetailView, ListView
 
 # Import local modules
 from .models import Post
 
 
+# Home page view function base
 def home(request) -> HttpResponse:
     """Home page view."""
     context: dict[str, BaseManager[Post]] = {"posts": Post.objects.all()}
@@ -16,6 +18,20 @@ def home(request) -> HttpResponse:
     return render(request=request, template_name="blog/home.html", context=context)
 
 
+# Home page view class base
+class PostListView(ListView):
+    model = Post
+    template_name = "blog/home.html"  # <app>/<model>_<viewtype>.html
+    context_object_name = "posts"
+    ordering: list[str] = ["-date_posted"]
+
+
+# Post detail page view
+class PostDetailView(DetailView):
+    model = Post
+
+
+# About page view function base
 def about(request) -> HttpResponse:
     """About page view."""
     return render(
